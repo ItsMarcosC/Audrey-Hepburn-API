7afa100415a22881775aee0a22e886820d5d1da7
Marcos Carvalho <marcosfilhosp@hotmail.com>
2022-05-13 16:23:58 -0300

    creates PORT dynamic

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3025
+const PORT = process.env.PORT || 3025
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Welcome to the Audrey Hepburn API!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
 })
